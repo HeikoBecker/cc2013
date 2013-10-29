1d126bdda65323e93edde9676a624a3774a6c693
Fabian Kosmale <fakos@t-online.de>
2013-10-29 21:41:41 +0100

    still not ready

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -5,43 +5,126 @@
 
 Lexer::Lexer(FILE* f)
 {
-	this->source = f;
+		this->source = f; 
+		this->keywords = std::unordered_set<std::string> {{
+				"_Alignas",
+						"_Alignof",
+						"_Atomic",
+						"_Bool",
+						"_Complex",
+						"_Generic",
+						"_Imaginary",
+						"_Noreturn",
+						"_Static_assert",
+						"_Thread_local",
+						"auto",
+						"break",
+						"case",
+						"char",
+						"const",
+						"continue",
+						"default",
+						"do",
+						"double",
+						"else",
+						"enum",
+						"extern",
+						"float",
+						"for",
+						"goto",
+						"if",
+						"inline",
+						"int",
+						"long",
+						"register",
+						"restrict",
+						"return",
+						"short",
+						"signed",
+						"sizeof",
+						"static",
+						"struct",
+						"switch",
+						"typedef",
+						"union",
+						"unsigned",
+						"void",
+						"volatile",
+						"while",
+		}};
 }
 
 std::vector<Token> Lexer::lex() {
-    std::vector<Token> result;
-    /* delim is the current token deliminator
-     * it is WHITESPACE, except when a (double) quote has been previously
-     * encountered 
-     */
-    Lexer::SearchedDelimeter delim = WHITESPACE;
-    int current;
-    std::stringstream curword;
-    while (current = fgetc(source) != EOF) {
-	    switch (delim) {
-		    case WHITESPACE:
-			    if (isspace(current)) {
-				    // if we had previous chars, we now got a token
-			    } else if ('\'' == current) {
-				    // a new character constant begins
-				    delim = SINGLEQUOTE;
-			    } else if ('\"' == current) {
-				    // a new string literal begins
-				    delim = DOUBLEQUOTE;
-			    }
-			    break;
-		    case SINGLEQUOTE:
-			    if ('\'' == current) {
-				    // got a character constant
-			    }
-			    break;
-		    case DOUBLEQUOTE:
-			    if ('\"' == current) {
-				    // got a string literal
-			    }
-			    break;
-	    }
-	    curword << static_cast<unsigned char>(current);
+		std::vector<Token> result;
+		/* delim is the current token deliminator
+		 * it is WHITESPACE, except when a (double) quote has been previously
+		 * encountered 
+		 */
+		Lexer::SearchedDelimeter delim = WHITESPACE;
+		int current;
+		std::stringstream curword;
+		while ((current = fgetc(source) != EOF)) {
+				/*if ((current == '\'' && delim == SINGLEQUOTE ) ||*/
+				/*(current == '\"' && delim == DOUBLEQUOTE)) {*/
+				/*// found string literal or character constant*/
+				/*}*/
+				if (delim != WHITESPACE) {
+						if (current == '\\') {
+								//start of escape sequence
+								if ((current = fgetc(source) == EOF)) {
+										// report error, got EOF while waiting for end
+										// of escape sequence
+								} else {
+										switch (current) { //TODO: implement this
+												case '\'':
+														break;
+												case '\"':
+														break;
+												case '\?':
+														break;
+												case '\\':
+														break;
+												case 'a':
+														break;
+												case 'b':
+														break;
+												case 'f':
+														break;
+												case 'n':
+														break;
+												case 'r':
+														break;
+												case 't':
+														break;
+												case 'v':
+														break;
+												default:
+														//report error
+														break;
 
-    }
-}
+										}
+								}
+						} else if (delim == SINGLEQUOTE && current == '\'') {
+								// end of character constant
+								delim = WHITESPACE;
+						} else if (delim == DOUBLEQUOTE && current == '\"') {
+								// end of string literal
+								delim = WHITESPACE;
+						} else {
+							// normal character
+						}
+				}
+				if (isspace(current)) {
+						// if we're not in a char constant or a string literal,
+						// whitespace doesn't matter
+						// found token if there was previous input
+						// remove consecutive whitespace
+						while ((current = fgetc(source) != EOF)) {
+								if (!isspace(current)) {
+										break;
+								}
+						}
+				}
+		}
+		return std::vector<Token> {};
+};
--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -1,15 +1,18 @@
 #ifndef LEXER_LEXER_H
 #define LEXER_LEXER_H
-struct FILE;
+
+#include <vector>
+#include <unordered_set>
+
 class Token;
 
 enum TokenType {
 		keyword = 0,
 		identifier = 1,
 		constant = 2,
-		string-literal = 3,
+		stringLiteral = 3,
 		punctuator = 4
-}
+};
 
 /*
  * \brief Class implementing the basic lexing functionality
@@ -25,56 +28,11 @@ class Lexer
 			WHITESPACE = 0,
 			SINGLEQUOTE = 1,
 			DOUBLEQUOTE = 2,
-		}
+		};
 
 	private:
 		FILE* source;	
-		static const_expr std::unordered_set<std::string> keywords {{
-			"_Alignas",
-			"_Alignof",
-			"_Atomic",
-			"_Bool",
-			"_Complex",
-			"_Generic",
-			"_Imaginary",
-			"_Noreturn",
-			"_Static_assert",
-			"_Thread_local",
-			"auto",
-			"break",
-			"case",
-			"char",
-			"const",
-			"continue",
-			"default",
-			"do",
-			"double",
-			"else",
-			"enum",
-			"extern",
-			"float",
-			"for",
-			"goto",
-			"if",
-			"inline",
-			"int",
-			"long",
-			"register",
-			"restrict",
-			"return",
-			"short",
-			"signed",
-			"sizeof",
-			"static",
-			"struct",
-			"switch",
-			"typedef",
-			"union",
-			"unsigned",
-			"void",
-			"volatile",
-			"while",
-		}};
-}
+		static std::unordered_set<std::string> keywords;
+};
 
 #endif
