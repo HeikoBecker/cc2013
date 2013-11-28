45e8517763f4b9973c31655ee5bbef386b1c5b59
Fabian Kosmale <fakos@t-online.de>
2013-11-28 18:54:14 +0100

    use move to make code more efficient

--- src/parser/parser.cc
+++ src/parser/parser.cc
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <cstdio>
 #include <cctype>
+#include <utility> // for std::move
 #include "parser.h"
 
 #define ABORT do {throw std::exception();} while (false)
@@ -11,7 +12,7 @@ using namespace Lexing;
 using namespace Parsing;
 
 // init parser
-Parser::Parser(vector<Token> tokens) : tokenList(tokens) {
+Parser::Parser(vector<Token> tokens) : tokenList(std::move(tokens)) {
   // should you really copy?
   // or is a reference enough
 }
