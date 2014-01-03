09e28985644ef1de61fbca0a1532205acdfb5b98
Patrick Klitzke <philologos14@gmail.com>
2014-01-03 22:04:29 +0100

    lot of failing tests

---
+++ tests/lexer-failing/constants_input/constant_input.c
@@ -0,0 +1,9 @@
+"String literal" 12345 "auto"
+1.25 0
+2.45E-2 1E4	3.5e+4
+"escape the \" quote!"
+00
+100
+0.1
+0.
+10.
---
+++ tests/lexer-failing/cr_linebreak/cr_linebreak.c
@@ -0,0 +1,4 @@
+/*DO NOT EDIT WITH GEDIT (or likewise editors). This file contains special line breaks, which will be fixed by such editors.*/int main(void) {
+	int x;
+	x = x+1;
+}
