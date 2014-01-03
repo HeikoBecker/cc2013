6ee8cc7015eea9d675ec4a517040414bb9f1c17c
Patrick Klitzke <philologos14@gmail.com>
2014-01-03 21:19:50 +0100

    lexer failing examples

---
+++ tests/lexer-failing/all_ops/all_ops.c
@@ -0,0 +1,6 @@
+/* Oops all my operators fell of the table an are lying in a big
+ * pile on the ground. The comment shows how the tokens are separated by maximum
+ * munch */
+/* [](){}... . -> ++ += + -- -= - && &= & || |= | *= * ~ != ! /= / %= % <<= >>
+ * << >>= > < >= == <= ^ ^= ? : ; = , */
+[](){}....->+++=+---=-&&&=&|||=|*=*~!=!/=/%=%<<=>><<>>=><>===<=^^=?:;=,
---
+++ tests/lexer-failing/comment_with_CR_only/comment_with_CR_only.c
@@ -0,0 +1,2 @@
+// CR nowcomment_with_CR_only int this_must_show_up;
+/* block comment with CR *////**/
