453aa074bd0a133a1f3416d53317acb2f1d144e6
Fabian Kosmale <fakos@t-online.de>
2014-01-05 13:22:39 +0100

    added more test cases

---
+++ tests/semantic/pass/recursion.c
@@ -0,0 +1,4 @@
+int fac(int n) {
+  if (n <= 0) return 1;
+  return fac(n-1)*n;
+}
---
+++ tests/semantic/pass/usageOfVariableFromOuterScope.c
@@ -0,0 +1,12 @@
+char x;
+
+int (main())
+{
+        int y;
+        {
+          int z;
+          (x = (y + z));
+        }
+        return y;
+}
+
