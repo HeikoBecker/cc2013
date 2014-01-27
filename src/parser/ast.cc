739a6a18f28009e7aa8a145e9460c4b31e158f1a
Fabian Kosmale <fakos@t-online.de>
2014-01-27 10:32:50 +0100

    pprinter: moved pprinting functions to pprinter.cc

--- src/parser/ast.cc
+++ src/parser/ast.cc
@@ -3,27 +3,6 @@
 #include "parser.h"
 #include "../utils/debug.h"
 
-bool g_skipNewLineBeforeBlockStatement = false; // TODO: FIXME: global variables are BAD!
-bool g_skipNewLineBeforeSelectionStatement = false; // TODO: FIXME: global variables are BAD!
-
-#define PRETTY_PRINT(X) void X::prettyPrint(const PrettyPrinter & pp, unsigned int indentLevel)
-/* Beware of macro magic
- * This prints the name of function calling PPRINT, it's argument and finally
- * any output produced by the function
- */
-#define PPRINT(X)  do { \
-  debug(PRINT_AST) << __PRETTY_FUNCTION__ << "\t: "\
-        << std::string(#X) << ":";\
-  pp.pprint((X), indentLevel);\
-} while(0);
-#define ADDINDENT()  do {indentLevel++;} while(0);
-#define REMOVEINDENT()  do {indentLevel--;} while(0);
-#define RESETINDENT() \
-  auto reset = indentLevel;\
-  indentLevel = 0;
-#define RESTOREINDENT() indentLevel = reset;
-
-
 using namespace Parsing;
 
 BinaryExpression::BinaryExpression(SubExpression lhs,
@@ -37,46 +16,9 @@ BinaryExpression::BinaryExpression(SubExpression lhs,
 {
 }
 
-PRETTY_PRINT(BinaryExpression)
-{
-  PPRINT('(');
-  PPRINT(this->lhs);
-  switch (this->op) {
-    case PunctuatorType::ARRAY_ACCESS:
-      PPRINT('[');
-      PPRINT(this->rhs);
-      PPRINT(']');
-      PPRINT(')');
-      return;
-      //break;
-    case PunctuatorType::ARROW:
-    case PunctuatorType::MEMBER_ACCESS:
-      PPRINT(op);
-      break;
-    default:
-      PPRINT(' ');
-      PPRINT(op);
-      PPRINT(' ');
-      break;
-  }
-  PPRINT(this->rhs);
-  PPRINT(')');
-}
-
 UnaryExpression::UnaryExpression(PunctuatorType op, SubExpression operand, Pos pos) :
   Expression(pos), operand(operand), op(op)
 {
-}
-
-PRETTY_PRINT(UnaryExpression)
-{
-  PPRINT('(');
-  PPRINT(this->op);
-  if (this->op == PunctuatorType::SIZEOF && !(std::dynamic_pointer_cast<SizeOfExpression>(operand))) {
-    PPRINT(' ');
-  }
-  PPRINT(this->operand);
-  PPRINT(')');
 }
 
 VariableUsage::VariableUsage(std::string name, Pos pos, 
@@ -94,43 +36,9 @@ Constant::Constant(std::string name, Pos pos)
   : Expression(pos), name(name) {}
 
 
-PRETTY_PRINT(VariableUsage)
-{
-  PPRINT(this->name);
-}
-
-PRETTY_PRINT(Literal)
-{
-  PPRINT(this->name);
-}
-
-PRETTY_PRINT(Constant)
-{
-  PPRINT(this->name);
-}
-
 FunctionCall::FunctionCall(SubExpression funcName,
                            std::vector<SubExpression> arguments, Pos pos)
         : Expression(pos), funcName(funcName), arguments(arguments) {;}
-
-PRETTY_PRINT(FunctionCall)
-{
-  PPRINT('(');
-  PPRINT(funcName);
-  PPRINT('(');
-  if (!arguments.empty()) {
-    auto size = arguments.size();
-      for (auto argument : arguments) {
-        PPRINT(argument);
-        if (--size != 0) {
-          PPRINT(',');
-          PPRINT(' ');
-        }
-      }
-  }
-  PPRINT(')');
-  PPRINT(')');
-}
 
 TernaryExpression::TernaryExpression(SubExpression condition, 
                                      SubExpression lhs, 
@@ -139,17 +47,6 @@ TernaryExpression::TernaryExpression(SubExpression condition,
      : Expression(pos), condition(condition), lhs(lhs), rhs(rhs)
 {
  //TODO: type checking
-}
-
-PRETTY_PRINT(TernaryExpression)
-{
-  PPRINT('(');
-  PPRINT(this->condition);
-  PPRINT(std::string(" ? "));
-  PPRINT(this->lhs);
-  PPRINT(std::string(" : "));
-  PPRINT(this->rhs);
-  PPRINT(')');
 }
 
 BasicType::BasicType(std::string type, Pos pos) : Type(pos)
@@ -175,22 +72,6 @@ string BasicType::toString() {
   }
 }
 
-PRETTY_PRINT(BasicType)
-{
-  switch(type) {
-    case INT:
-      PPRINT(std::string("int"));
-      break;
-    case CHAR:
-      PPRINT(std::string("char"));
-      break;
-    default :
-      /*case VOID: */
-      PPRINT(std::string("void"));
-      break;
-  }
-}
-
 StructType::StructType(std::string name, Pos pos) : Type(pos), name(name) {
   // default string is empty
   content = StructContent();
@@ -209,79 +90,10 @@ StructType::StructType(std::string name, StructContent content, Pos pos)
   hasDeclaration = true;
 }
 
-PRETTY_PRINT(StructType) {
-  // TODO: this looks way too complicated
-  PPRINT(std::string("struct "));
-
-  if (name.length() > 0) {
-    PPRINT(name);
-  }
-
-  if (!content.empty()) {
-    PPRINT('\n');
-    PPRINT('{');
-    ADDINDENT();
-
-    for (auto typeSubDeclarationPair : content) {
-      PPRINT('\n');
-      PPRINT(typeSubDeclarationPair.first);
-      if (!typeSubDeclarationPair.second.empty()) {
-        PPRINT(' '); // print space between type and following stuff
-      } else {
-        PPRINT(';');
-      }
-      for (auto subDeclarationPair : typeSubDeclarationPair.second) {
-        PPRINT(subDeclarationPair.first);
-        if (subDeclarationPair.second) {
-          //PPRINT(' '); //TODO: this seems wrong
-          PPRINT(subDeclarationPair.second);
-        }
-        PPRINT(';'); // <- FIXME: no idea if this belongs here, but probably correct
-      }
-    }
-    REMOVEINDENT();
-    PPRINT('\n');
-    PPRINT('}');
-  }
-}
-
 CompoundStatement::CompoundStatement(std::vector<BlockItem> subStatements, Pos pos)
   : Statement(pos), subStatements(std::move(subStatements))
 {
 
-}
-
-PRETTY_PRINT(CompoundStatement) {
-  // TODO: unfinished, add special case for last statement regarding newline
-  if (!g_skipNewLineBeforeBlockStatement) {
-    PPRINT('\n');
-  } else {
-    PPRINT(' ');
-    g_skipNewLineBeforeBlockStatement = false; //FIXME: global variable!
-  }
-  PPRINT('{');
-  ADDINDENT();
-  for (auto statement : subStatements) {
-    PPRINT(statement);
-  }
-  REMOVEINDENT();
-  PPRINT('\n');
-  PPRINT('}');
-}
-
-PRETTY_PRINT(Pointer) {
-  for(int n=0; n<counter; n++) {
-    PPRINT('*');
-  }
-}
-
-PRETTY_PRINT(ExpressionStatement) {
-  // TODO : check whether this is working
-  PPRINT('\n');
-  if (expression != NULL) {
-    PPRINT(expression);
-  }
-  PPRINT(';');
 }
 
 SelectionStatement::SelectionStatement(SubExpression ex,
@@ -304,140 +116,6 @@ SelectionStatement::SelectionStatement(
 }
 
 
-PRETTY_PRINT(SelectionStatement) {
-  auto suppressedIndent = false;
-  if (g_skipNewLineBeforeSelectionStatement) {
-    g_skipNewLineBeforeSelectionStatement = false;
-    PPRINT(' ');
-  } else {
-    PPRINT('\n');
-  }
-  PPRINT(std::string("if ("));
-  PPRINT(expression);
-  PPRINT(')');
-  if (std::dynamic_pointer_cast<CompoundStatement>(ifStatement)) {
-    suppressedIndent = true;
-    g_skipNewLineBeforeBlockStatement = true;
-  }
-  if (!suppressedIndent) {
-    ADDINDENT();
-  }
-  PPRINT(ifStatement);
-  if (!suppressedIndent) {
-    REMOVEINDENT();
-  }
-
-  if (elseStatement) {
-    if (suppressedIndent) {
-      PPRINT(' ');
-      suppressedIndent = false;
-    } else {
-      PPRINT('\n');
-    }
-    PPRINT(std::string("else"));
-    // don't set a new indent level if we have a selection statement
-    if (std::dynamic_pointer_cast<SelectionStatement>(elseStatement)) {
-      g_skipNewLineBeforeSelectionStatement = true;
-      suppressedIndent = true;
-    } else if (std::dynamic_pointer_cast<CompoundStatement>(elseStatement)) {
-      suppressedIndent = true;
-      g_skipNewLineBeforeBlockStatement = true;
-    } else {
-      ADDINDENT();
-    }
-    PPRINT(elseStatement);
-    if (!suppressedIndent) {
-      REMOVEINDENT(); // only remove indentLevel if we have added one
-    }
-  }
-}
-
-PRETTY_PRINT(GotoStatement) {
-  PPRINT('\n');
-  PPRINT(std::string("goto "));
-  PPRINT(label);
-  PPRINT(';');
-}
-
-PRETTY_PRINT(ContinueStatement) {
-  PPRINT('\n');
-  PPRINT(std::string("continue;"));
-}
-
-PRETTY_PRINT(BreakStatement) {
-  PPRINT('\n');
-  PPRINT(std::string("break;"));
-}
-
-PRETTY_PRINT(ReturnStatement) {
-  PPRINT('\n');
-  PPRINT(std::string("return"));
-  if (expression != NULL) {
-    PPRINT(' ');
-    PPRINT(expression);
-  }
-  PPRINT(';');
-}
-
-PRETTY_PRINT(IterationStatement) {
-  PPRINT('\n');
-  if (kind == WHILE) {
-    PPRINT(std::string("while ("));
-    PPRINT(expression);
-    PPRINT(std::string(")"));
-    if (std::dynamic_pointer_cast<CompoundStatement>(statement)) {
-      g_skipNewLineBeforeBlockStatement = true;
-      PPRINT(statement);
-    } else {
-      ADDINDENT();
-      PPRINT(statement);
-      REMOVEINDENT();
-    }
-  } else { // kind == DO
-    PPRINT(std::string("do "));
-    PPRINT(statement);
-    PPRINT(std::string("while ("));
-    PPRINT(expression);
-    PPRINT(std::string(");"));
-  }
-}
-
-PRETTY_PRINT(LabeledStatement) {
-  RESETINDENT();
-  PPRINT('\n');
-  PPRINT(name);
-  PPRINT(std::string(":"));
-  RESTOREINDENT();
-  if (std::dynamic_pointer_cast<LabeledStatement>(statement)) {
-    PPRINT(statement);
-  } else {
-    PPRINT(statement);
-  }
-}
-
-PRETTY_PRINT(IdentifierList) {
-  PPRINT(nameList[0]);
-  for(int n = 1; n < (int) nameList.size(); n++) {
-    PPRINT(',');
-    PPRINT(nameList[n]);
-  }
-}
-
-PRETTY_PRINT(Declarator)
-{
-  // TODO : unfinished, probably also broken
-  for (auto i = this->pointerCounter; i>0; --i) {
-    PPRINT('(');
-    PPRINT('*');
-  }
-  if (directDeclarator) {
-    PPRINT(this->directDeclarator);
-  }
-  for (auto i = this->pointerCounter; i>0; --i) {
-    PPRINT(')');
-  }
-}
-
 Declaration::Declaration(TypeNode t, SubDeclarator declarator, Pos pos, shared_ptr<SemanticTree> semanticTree)
   : AstNode(pos), type(t),declarator(declarator), semanticTree(semanticTree) {
     semanticTree->addDeclaration(type, declarator, pos);
@@ -446,17 +124,6 @@ Declaration::Declaration(TypeNode t, SubDeclarator declarator, Pos pos, shared_p
 
 Declaration::Declaration(TypeNode t, Pos pos)
   : AstNode(pos), type(t){}
-
-PRETTY_PRINT(Declaration)
-{
-  PPRINT('\n');
-  PPRINT(type);
-  PPRINT(' ');
-  if (declarator) {
-    PPRINT(declarator);
-  }
-  PPRINT(';');
-}
 
 
 ExternalDeclaration::ExternalDeclaration(TypeNode type,
@@ -501,53 +168,6 @@ TranslationUnit::TranslationUnit(
   }
 }
 
-PRETTY_PRINT(TranslationUnit) {
-  auto size = externalDeclarations.size();
-  for(auto externalDeclaration : externalDeclarations) {
-    PPRINT(externalDeclaration);
-    if (size > 1) {
-      PPRINT('\n');
-      size--;
-    }
-  }
-}
-
-PRETTY_PRINT(ExternalDeclaration) {
-  /*TODO: unfinished */
-  PPRINT(this->type);
-  if (this->declarator) {
-    PPRINT(' ');
-    if (compoundStatement) {
-      PPRINT('('); // <- WTF, that's a really coding style
-    }
-    PPRINT(this->declarator);
-    if (compoundStatement) {
-      PPRINT(')');
-    }
-    if (this->compoundStatement) {
-      PPRINT(this->compoundStatement);
-      PPRINT('\n');
-      debug(PARSER) << "ExternalDeclaration END";
-      return; // so that we don't print a semicolon
-    }
-  }
-  // TODO: move semicolon printing to declarations
-  PPRINT(';'); // declarations end with an ;
-  debug(PARSER) << "ExternalDeclaration END";
-  PPRINT('\n');
-}
-
-PRETTY_PRINT(IdentifierDirectDeclarator) {
-  /*TODO: unfinished*/
-  PPRINT(this->identifier);
-  auto numDiDeHelp = help.size();
-  // TODO: we might need to print helpers which are not the last one in
-  // parentheses
-  while (numDiDeHelp) {
-    PPRINT(help.at(--numDiDeHelp));
-  }
-}
-
 Parameter::Parameter(TypeNode type, SubDeclarator declarator, Pos pos)
   : AstNode(pos), type(type), declarator(declarator) {
   
@@ -556,35 +176,6 @@ Parameter::Parameter(TypeNode type, SubDeclarator declarator, Pos pos)
 Parameter::Parameter(TypeNode type, Pos pos)
   : AstNode(pos), type(type) {
   
-}
-
-PRETTY_PRINT(Parameter) {
-  /*TODO: unfinished*/
-  PPRINT(type);
-  if (declarator) {
-    PPRINT(' ');
-    PPRINT(declarator);
-  }
-}
-
-
-PRETTY_PRINT(DeclaratorDirectDeclarator)
-{
-  // TODO: broken
-  size_t numDiDeHelp = help.size();
-  if (numDiDeHelp > 0) {
-    PPRINT('(');
-  }
-  PPRINT(declarator);
-  if (numDiDeHelp > 0) {
-    PPRINT(help.at(--numDiDeHelp));
-    while (numDiDeHelp) {
-      PPRINT('(');
-      PPRINT(help.at(--numDiDeHelp));
-      PPRINT(')');
-    }
-    PPRINT(')');
-  }
 }
 
 
@@ -616,48 +207,9 @@ DirectDeclaratorHelp::DirectDeclaratorHelp(SubIdentifierList idList, Pos pos)
 }
 
 
-PRETTY_PRINT(DirectDeclaratorHelp)
-{
-  switch (helperType) {
-    case EPSILON:
-      // TODO: the EPSILON case should not be necessary anymore
-      debug(PARSER) << "should never happen!\n";
-      PPRINT('(');
-      PPRINT(')');
-      return;
-    case IDENTIFIERLIST:
-      PPRINT(idList);
-    case EMPTYLIST:
-      PPRINT('(');
-      PPRINT(')');
-      break;
-    case PARAMETERLIST:
-      PPRINT('(');
-      auto size = paramList.size();
-      for (auto parameter: paramList) {
-        PPRINT(parameter);
-        size--;
-        if (size > 0) {
-          PPRINT(std::string(", "));
-        }
-      }
-      PPRINT(')');
-  }
-}  
-
 SizeOfExpression::SizeOfExpression(std::pair<TypeNode, SubDeclarator> operand, Pos pos)
   : Expression(pos), operand(operand) 
 {
-}
-
-PRETTY_PRINT(SizeOfExpression)
-{
-  PPRINT('(');
-  PPRINT(operand.first);
-  if (operand.second) {
-    PPRINT(operand.second);
-  }
-  PPRINT(')');
 }
 
 ReturnStatement::ReturnStatement(Pos pos) : JumpStatement(pos) {}
@@ -779,8 +331,3 @@ IdentifierList::IdentifierList(std::vector<std::string > list, Pos pos)
 
 Declarator::Declarator(int cnt, SubDirectDeclarator ast, Pos pos)
   : AstNode(pos), pointerCounter(cnt), directDeclarator(ast) {}
-
-#undef PPRINT
-#undef ADDINDENT
-#undef REMOVEINDENT
-#undef PRETTY_PRINT
--- src/parser/pprinter.cc
+++ src/parser/pprinter.cc
@@ -1,6 +1,461 @@
 #include <iostream>
+#include "ast.h"
 #include "pprinter.h"
 
 using namespace Parsing;
 
 PrettyPrinter::PrettyPrinter() {}
+
+bool g_skipNewLineBeforeBlockStatement = false; // TODO: FIXME: global variables are BAD!
+bool g_skipNewLineBeforeSelectionStatement = false; // TODO: FIXME: global variables are BAD!
+
+#define PRETTY_PRINT(X) void X::prettyPrint(const PrettyPrinter & pp, unsigned int indentLevel)
+/* Beware of macro magic
+ * This prints the name of function calling PPRINT, it's argument and finally
+ * any output produced by the function
+ */
+#define PPRINT(X)  do { \
+  debug(PRINT_AST) << __PRETTY_FUNCTION__ << "\t: "\
+        << std::string(#X) << ":";\
+  pp.pprint((X), indentLevel);\
+} while(0);
+#define ADDINDENT()  do {indentLevel++;} while(0);
+#define REMOVEINDENT()  do {indentLevel--;} while(0);
+#define RESETINDENT() \
+  auto reset = indentLevel;\
+  indentLevel = 0;
+#define RESTOREINDENT() indentLevel = reset;
+
+
+PRETTY_PRINT(BinaryExpression)
+{
+  PPRINT('(');
+  PPRINT(this->lhs);
+  switch (this->op) {
+    case PunctuatorType::ARRAY_ACCESS:
+      PPRINT('[');
+      PPRINT(this->rhs);
+      PPRINT(']');
+      PPRINT(')');
+      return;
+      //break;
+    case PunctuatorType::ARROW:
+    case PunctuatorType::MEMBER_ACCESS:
+      PPRINT(op);
+      break;
+    default:
+      PPRINT(' ');
+      PPRINT(op);
+      PPRINT(' ');
+      break;
+  }
+  PPRINT(this->rhs);
+  PPRINT(')');
+}
+
+PRETTY_PRINT(UnaryExpression)
+{
+  PPRINT('(');
+  PPRINT(this->op);
+  if (this->op == PunctuatorType::SIZEOF && !(std::dynamic_pointer_cast<SizeOfExpression>(operand))) {
+    PPRINT(' ');
+  }
+  PPRINT(this->operand);
+  PPRINT(')');
+}
+
+PRETTY_PRINT(VariableUsage)
+{
+  PPRINT(this->name);
+}
+
+PRETTY_PRINT(Literal)
+{
+  PPRINT(this->name);
+}
+
+PRETTY_PRINT(Constant)
+{
+  PPRINT(this->name);
+}
+
+PRETTY_PRINT(FunctionCall)
+{
+  PPRINT('(');
+  PPRINT(funcName);
+  PPRINT('(');
+  if (!arguments.empty()) {
+    auto size = arguments.size();
+      for (auto argument : arguments) {
+        PPRINT(argument);
+        if (--size != 0) {
+          PPRINT(',');
+          PPRINT(' ');
+        }
+      }
+  }
+  PPRINT(')');
+  PPRINT(')');
+}
+
+PRETTY_PRINT(TernaryExpression)
+{
+  PPRINT('(');
+  PPRINT(this->condition);
+  PPRINT(std::string(" ? "));
+  PPRINT(this->lhs);
+  PPRINT(std::string(" : "));
+  PPRINT(this->rhs);
+  PPRINT(')');
+}
+
+PRETTY_PRINT(BasicType)
+{
+  switch(type) {
+    case INT:
+      PPRINT(std::string("int"));
+      break;
+    case CHAR:
+      PPRINT(std::string("char"));
+      break;
+    default :
+      /*case VOID: */
+      PPRINT(std::string("void"));
+      break;
+  }
+}
+
+PRETTY_PRINT(StructType) {
+  // TODO: this looks way too complicated
+  PPRINT(std::string("struct "));
+
+  if (name.length() > 0) {
+    PPRINT(name);
+  }
+
+  if (!content.empty()) {
+    PPRINT('\n');
+    PPRINT('{');
+    ADDINDENT();
+
+    for (auto typeSubDeclarationPair : content) {
+      PPRINT('\n');
+      PPRINT(typeSubDeclarationPair.first);
+      if (!typeSubDeclarationPair.second.empty()) {
+        PPRINT(' '); // print space between type and following stuff
+      } else {
+        PPRINT(';');
+      }
+      for (auto subDeclarationPair : typeSubDeclarationPair.second) {
+        PPRINT(subDeclarationPair.first);
+        if (subDeclarationPair.second) {
+          //PPRINT(' '); //TODO: this seems wrong
+          PPRINT(subDeclarationPair.second);
+        }
+        PPRINT(';'); // <- FIXME: no idea if this belongs here, but probably correct
+      }
+    }
+    REMOVEINDENT();
+    PPRINT('\n');
+    PPRINT('}');
+  }
+}
+
+
+PRETTY_PRINT(CompoundStatement) {
+  // TODO: unfinished, add special case for last statement regarding newline
+  if (!g_skipNewLineBeforeBlockStatement) {
+    PPRINT('\n');
+  } else {
+    PPRINT(' ');
+    g_skipNewLineBeforeBlockStatement = false; //FIXME: global variable!
+  }
+  PPRINT('{');
+  ADDINDENT();
+  for (auto statement : subStatements) {
+    PPRINT(statement);
+  }
+  REMOVEINDENT();
+  PPRINT('\n');
+  PPRINT('}');
+}
+
+PRETTY_PRINT(Pointer) {
+  for(int n=0; n<counter; n++) {
+    PPRINT('*');
+  }
+}
+
+PRETTY_PRINT(ExpressionStatement) {
+  // TODO : check whether this is working
+  PPRINT('\n');
+  if (expression != NULL) {
+    PPRINT(expression);
+  }
+  PPRINT(';');
+}
+
+PRETTY_PRINT(SelectionStatement) {
+  auto suppressedIndent = false;
+  if (g_skipNewLineBeforeSelectionStatement) {
+    g_skipNewLineBeforeSelectionStatement = false;
+    PPRINT(' ');
+  } else {
+    PPRINT('\n');
+  }
+  PPRINT(std::string("if ("));
+  PPRINT(expression);
+  PPRINT(')');
+  if (std::dynamic_pointer_cast<CompoundStatement>(ifStatement)) {
+    suppressedIndent = true;
+    g_skipNewLineBeforeBlockStatement = true;
+  }
+  if (!suppressedIndent) {
+    ADDINDENT();
+  }
+  PPRINT(ifStatement);
+  if (!suppressedIndent) {
+    REMOVEINDENT();
+  }
+
+  if (elseStatement) {
+    if (suppressedIndent) {
+      PPRINT(' ');
+      suppressedIndent = false;
+    } else {
+      PPRINT('\n');
+    }
+    PPRINT(std::string("else"));
+    // don't set a new indent level if we have a selection statement
+    if (std::dynamic_pointer_cast<SelectionStatement>(elseStatement)) {
+      g_skipNewLineBeforeSelectionStatement = true;
+      suppressedIndent = true;
+    } else if (std::dynamic_pointer_cast<CompoundStatement>(elseStatement)) {
+      suppressedIndent = true;
+      g_skipNewLineBeforeBlockStatement = true;
+    } else {
+      ADDINDENT();
+    }
+    PPRINT(elseStatement);
+    if (!suppressedIndent) {
+      REMOVEINDENT(); // only remove indentLevel if we have added one
+    }
+  }
+}
+
+PRETTY_PRINT(GotoStatement) {
+  PPRINT('\n');
+  PPRINT(std::string("goto "));
+  PPRINT(label);
+  PPRINT(';');
+}
+
+PRETTY_PRINT(ContinueStatement) {
+  PPRINT('\n');
+  PPRINT(std::string("continue;"));
+}
+
+PRETTY_PRINT(BreakStatement) {
+  PPRINT('\n');
+  PPRINT(std::string("break;"));
+}
+
+PRETTY_PRINT(ReturnStatement) {
+  PPRINT('\n');
+  PPRINT(std::string("return"));
+  if (expression != NULL) {
+    PPRINT(' ');
+    PPRINT(expression);
+  }
+  PPRINT(';');
+}
+
+PRETTY_PRINT(IterationStatement) {
+  PPRINT('\n');
+  if (kind == WHILE) {
+    PPRINT(std::string("while ("));
+    PPRINT(expression);
+    PPRINT(std::string(")"));
+    if (std::dynamic_pointer_cast<CompoundStatement>(statement)) {
+      g_skipNewLineBeforeBlockStatement = true;
+      PPRINT(statement);
+    } else {
+      ADDINDENT();
+      PPRINT(statement);
+      REMOVEINDENT();
+    }
+  } else { // kind == DO
+    PPRINT(std::string("do "));
+    PPRINT(statement);
+    PPRINT(std::string("while ("));
+    PPRINT(expression);
+    PPRINT(std::string(");"));
+  }
+}
+
+PRETTY_PRINT(LabeledStatement) {
+  RESETINDENT();
+  PPRINT('\n');
+  PPRINT(name);
+  PPRINT(std::string(":"));
+  RESTOREINDENT();
+  if (std::dynamic_pointer_cast<LabeledStatement>(statement)) {
+    PPRINT(statement);
+  } else {
+    PPRINT(statement);
+  }
+}
+
+PRETTY_PRINT(IdentifierList) {
+  PPRINT(nameList[0]);
+  for(int n = 1; n < (int) nameList.size(); n++) {
+    PPRINT(',');
+    PPRINT(nameList[n]);
+  }
+}
+
+PRETTY_PRINT(Declarator)
+{
+  // TODO : unfinished, probably also broken
+  for (auto i = this->pointerCounter; i>0; --i) {
+    PPRINT('(');
+    PPRINT('*');
+  }
+  if (directDeclarator) {
+    PPRINT(this->directDeclarator);
+  }
+  for (auto i = this->pointerCounter; i>0; --i) {
+    PPRINT(')');
+  }
+}
+
+PRETTY_PRINT(Declaration)
+{
+  PPRINT('\n');
+  PPRINT(type);
+  PPRINT(' ');
+  if (declarator) {
+    PPRINT(declarator);
+  }
+  PPRINT(';');
+}
+
+PRETTY_PRINT(TranslationUnit) {
+  auto size = externalDeclarations.size();
+  for(auto externalDeclaration : externalDeclarations) {
+    PPRINT(externalDeclaration);
+    if (size > 1) {
+      PPRINT('\n');
+      size--;
+    }
+  }
+}
+
+PRETTY_PRINT(ExternalDeclaration) {
+  /*TODO: unfinished */
+  PPRINT(this->type);
+  if (this->declarator) {
+    PPRINT(' ');
+    if (compoundStatement) {
+      PPRINT('('); // <- WTF, that's a really coding style
+    }
+    PPRINT(this->declarator);
+    if (compoundStatement) {
+      PPRINT(')');
+    }
+    if (this->compoundStatement) {
+      PPRINT(this->compoundStatement);
+      PPRINT('\n');
+      debug(PARSER) << "ExternalDeclaration END";
+      return; // so that we don't print a semicolon
+    }
+  }
+  // TODO: move semicolon printing to declarations
+  PPRINT(';'); // declarations end with an ;
+  debug(PARSER) << "ExternalDeclaration END";
+  PPRINT('\n');
+}
+
+PRETTY_PRINT(IdentifierDirectDeclarator) {
+  /*TODO: unfinished*/
+  PPRINT(this->identifier);
+  auto numDiDeHelp = help.size();
+  // TODO: we might need to print helpers which are not the last one in
+  // parentheses
+  while (numDiDeHelp) {
+    PPRINT(help.at(--numDiDeHelp));
+  }
+}
+
+PRETTY_PRINT(Parameter) {
+  /*TODO: unfinished*/
+  PPRINT(type);
+  if (declarator) {
+    PPRINT(' ');
+    PPRINT(declarator);
+  }
+}
+
+
+PRETTY_PRINT(DeclaratorDirectDeclarator)
+{
+  // TODO: broken
+  size_t numDiDeHelp = help.size();
+  if (numDiDeHelp > 0) {
+    PPRINT('(');
+  }
+  PPRINT(declarator);
+  if (numDiDeHelp > 0) {
+    PPRINT(help.at(--numDiDeHelp));
+    while (numDiDeHelp) {
+      PPRINT('(');
+      PPRINT(help.at(--numDiDeHelp));
+      PPRINT(')');
+    }
+    PPRINT(')');
+  }
+}
+
+PRETTY_PRINT(DirectDeclaratorHelp)
+{
+  switch (helperType) {
+    case EPSILON:
+      // TODO: the EPSILON case should not be necessary anymore
+      debug(PARSER) << "should never happen!\n";
+      PPRINT('(');
+      PPRINT(')');
+      return;
+    case IDENTIFIERLIST:
+      PPRINT(idList);
+    case EMPTYLIST:
+      PPRINT('(');
+      PPRINT(')');
+      break;
+    case PARAMETERLIST:
+      PPRINT('(');
+      auto size = paramList.size();
+      for (auto parameter: paramList) {
+        PPRINT(parameter);
+        size--;
+        if (size > 0) {
+          PPRINT(std::string(", "));
+        }
+      }
+      PPRINT(')');
+  }
+}  
+
+PRETTY_PRINT(SizeOfExpression)
+{
+  PPRINT('(');
+  PPRINT(operand.first);
+  if (operand.second) {
+    PPRINT(operand.second);
+  }
+  PPRINT(')');
+}
+
+#undef PPRINT
+#undef ADDINDENT
+#undef REMOVEINDENT
+#undef PRETTY_PRINT
