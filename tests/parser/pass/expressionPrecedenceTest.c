21965060667e6cbd81ab0f0dd20758e9b2a69f0c
Fabian Kosmale <0inkane@googlemail.com>
2014-02-13 14:08:43 +0100

    tests: made test valid C

--- tests/parser/pass/expressionPrecedenceTest.c
+++ tests/parser/pass/expressionPrecedenceTest.c
@@ -1,3 +1,4 @@
+int a; int b; int d; int e;
 int main(void) {
   a*b+d+e;
   return 0;
--- tests/parser/pass/expressionWithFunctionCall.c
+++ tests/parser/pass/expressionWithFunctionCall.c
@@ -1,5 +1,9 @@
+int a;
+int b(char, char, char);
+int c(int);
+char d();
 int main() {
   if (a<b(1,2,3)) {
-  if (a() == b(1)) {;};
+  if (d() == c(1)) {;};
   }
 }
