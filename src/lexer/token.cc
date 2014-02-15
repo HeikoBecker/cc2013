5423adfaee736897e8c70125d5561f6b8008cbbd
Fabian Kosmale <fakos@t-online.de>
2014-02-15 20:08:52 +0100

    token.cc: fix pedantic error

--- src/lexer/token.cc
+++ src/lexer/token.cc
@@ -18,7 +18,7 @@ Token::Token(TokenType type, Pos posinfo, std::string value)
 }
 
 ConstantToken::ConstantToken(Pos posinfo, std::string value, ConstantType ct) 
-  : Token(TokenType::CONSTANT, posinfo, value), type(ct) {};
+  : Token(TokenType::CONSTANT, posinfo, value), type(ct) {}
 
 
 PunctuatorToken::PunctuatorToken(TokenType type, Pos posinfo, std::string value)
