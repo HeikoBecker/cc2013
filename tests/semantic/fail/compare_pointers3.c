0e760961504339e7113c9a9409c04e47920f22ad
Fabian Kosmale <fakos@t-online.de>
2014-02-21 19:34:22 +0100

    tests: more pointer comparision tests

---
+++ tests/semantic/fail/compare_pointers3.c
@@ -0,0 +1,11 @@
+struct S {int i;};
+struct T {int i;};
+
+int main(void) {
+  struct S* sptr;
+  struct T* tptr;
+  if (sptr != tptr) {
+    return 0;
+  }
+  return 0;
+}
---
+++ tests/semantic/fail/compare_pointers4.c
@@ -0,0 +1,11 @@
+struct S {int i;};
+struct T {int i;};
+
+int main(void) {
+  struct S* s;
+  struct T* t;
+  if (s != t) {
+    return 0;
+  }
+  return 0;
+}
