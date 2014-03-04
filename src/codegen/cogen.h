7a087c9073cc3e1511abdf2af85c6057988f4f3d
Fabian Kosmale <0inkane@googlemail.com>
2014-03-04 10:17:05 +0100

    cleanup: reduce number of included headers in cogen

--- src/codegen/cogen.h
+++ src/codegen/cogen.h
@@ -1,16 +1,6 @@
 #ifndef COGEN_H
 #define COGEN_H
 #include <memory>
-#include <string>
-
-#include "llvm/IR/Module.h"                /* Module */
-#include "llvm/IR/Function.h"              /* Function */
-#include "llvm/IR/Constant.h"              /* Constant::getNullValue */
-#include "llvm/IR/IRBuilder.h"             /* IRBuilder */
-#include "llvm/IR/LLVMContext.h"           /* LLVMContext */
-#include "llvm/IR/GlobalValue.h"           /* GlobaleVariable, LinkageTypes */
-#include "llvm/Analysis/Verifier.h"        /* verifyFunction, verifyModule */
-#include "llvm/Support/raw_ostream.h"
 
 namespace Parsing {
   class AstNode;
