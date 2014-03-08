29721947451423a851cff912aa9e69117f6e175d
Heiko Becker <heiko.becker@rocketmail.com>
2014-03-08 18:32:39 +0100

    master:added backend sizeof test

---
+++ tests/backend/pass/sizeof1.c
@@ -0,0 +1,10 @@
+int main (void){
+        int c;
+c=        sizeof(int);
+c=        sizeof(char);
+  c=      sizeof(int *);
+   c=     sizeof(char *);
+    c=    sizeof(int (*) (void));
+     c=   sizeof(int (*)(void) (int (*) ( char * (*) (int, char*, int***))));
+        return 1;
+}
