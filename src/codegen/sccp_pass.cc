f0e6c8cce653bb8401e596e58632837ef38751b8
Heiko Becker <heiko.becker@rocketmail.com>
2014-03-08 13:30:52 +0100

    master: added draft for transition function

--- src/codegen/sccp_pass.cc
+++ src/codegen/sccp_pass.cc
@@ -1,5 +1,6 @@
 #include "sccp_pass.h"
 
+#include "../utils/util.h"
 
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
@@ -10,6 +11,8 @@
 #include <queue>
 #include <map>
 #include <algorithm>
+
+#define TRANSITION(X, Y) void Transition::X(Y)
 
 using namespace llvm;
 
@@ -34,9 +37,9 @@ bool SCCP_Pass::runOnFunction(llvm::Function &F) {
   //}
  
   //Initialize the Transition object
-  Transition transMngr = Transition(F.getEntryBlock(), ValueMapping, BlockMapping);
+  Transition transMngr = Transition(&F.getEntryBlock(), ValueMapping, BlockMapping);
   
-  llvm::BasicBlock& curr = F.getEntryBlock();
+  llvm::BasicBlock* curr = &F.getEntryBlock();
 
   do{
   //FIXME: Iterate over instructions of block here!
@@ -47,3 +50,58 @@ bool SCCP_Pass::runOnFunction(llvm::Function &F) {
 
 char SCCP_Pass::ID = 0;
 static RegisterPass<SCCP_Pass> X("sccp", "SCCP Pass", false, false);
+
+//##############################################################################
+//##                              Transition Class                            ##
+//##############################################################################
+
+Transition::Transition( llvm::BasicBlock *entryBlock,
+                        std::map<llvm::Value*, ConstantLattice>& constantTable,
+                        std::map<llvm::BasicBlock*, Reachability>& blockTable):
+                        constantTable(constantTable), blockTable(blockTable) {
+ //FIXME: Do we need to put all blocks in the queue to visit them once?
+ currBlock = entryBlock;
+}
+
+/*
+ * Removes the first element from the queue to give it to the sccppass to do the
+ * work. As a side effect, the queues size is decreased by one when the element
+ * is removed.
+ */
+llvm::BasicBlock* Transition::getNextBlock() {
+  //save the first block to return it
+  llvm::BasicBlock *res = workQueue.front();
+  workQueue.pop(); //remove it from the queue
+  currBlock = res;
+  return res;
+}
+
+TRANSITION(visitBinaryOperator, llvm::BinaryOperator& binOp) {
+  UNUSED(binOp);
+  return;
+}
+
+TRANSITION(visitCastInst, llvm::CastInst &cast) {
+  UNUSED(cast);
+  return;
+}
+
+TRANSITION(visitICmpInst, llvm::ICmpInst &cmp){
+  UNUSED(cmp);
+  return;
+}
+
+TRANSITION(visitPHINode, llvm::PHINode &phi){
+  UNUSED(phi);
+  return;
+}
+
+TRANSITION(visitBranchInst, llvm::BranchInst &branch){
+  UNUSED(branch);
+  return;
+}
+
+TRANSITION(visitReturnInst, llvm::ReturnInst &ret){
+  UNUSED(ret);
+  return;
+}
--- src/codegen/sccp_pass.h
+++ src/codegen/sccp_pass.h
@@ -34,20 +34,35 @@ struct SCCP_Pass : public llvm::FunctionPass {
   bool runOnFunction(llvm::Function &F) override;
 };
 
-struct Transition: public llvm::InstVisitor<Transition> {
+struct Transition: public llvm::InstVisitor<Transition, void> {
   
   std::map<llvm::Value*, ConstantLattice> constantTable;
   std::map<llvm::BasicBlock*, Reachability> blockTable;
 
-  Transition(llvm::BasicBlock& entryBlock,
-std::map<llvm::Value*, ConstantLattice>&  constantTable,
+  Transition(llvm::BasicBlock* entryBlock,
+  std::map<llvm::Value*, ConstantLattice>&  constantTable,
   std::map<llvm::BasicBlock*, Reachability>& blockTable);
 
   //FIXME: Maybe pass the queue to the sccp_pass instead of forcing invocation
   //of method
-  llvm::BasicBlock& getNextBlock();
-  //override each instruction here!
+  llvm::BasicBlock* getNextBlock();
+  
+  //override each instruction here 
+  //BinaryOperator
+  void visitBinaryOperator (llvm::BinaryOperator& binOp);
+  //CastInst
+  void visitCastInst(llvm::CastInst& cast);
+  //ICmpInst
+  void visitICmpInst(llvm::ICmpInst& icmp);
+  //PHINode
+  void visitPHINode(llvm::PHINode& phi);
+  //BranchInst
+  void visitBranchInst(llvm::BranchInst& branch);
+  //ReturnInst
+  void visitReturnInst(llvm::ReturnInst& ret);
   
 private:
   std::queue<llvm::BasicBlock*> workQueue;
+  llvm::BasicBlock* currBlock; //to save the current block for equality checking
+                               //to do queue management
 };
